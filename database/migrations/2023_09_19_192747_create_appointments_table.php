706654a31885ef2743cff41b3f73a150937b58f6
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-09-24 00:00:30 +0200

    Create appointments migration

---
+++ database/migrations/2023_09_19_192747_create_appointments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('patient_id');
+            $table->string('doctor_id');
+            $table->string('app_time');
+            $table->string('app_date');
+            $table->string('purpose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
