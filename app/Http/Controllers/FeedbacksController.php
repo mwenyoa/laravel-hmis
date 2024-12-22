066555c6e3199215ceff9afd14251bd12160a726
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-22 14:19:59 +0200

    Create feedback API resourceful controller

---
+++ app/Http/Controllers/FeedbacksController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feedback;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+
+class FeedbacksController extends Controller
+{
+
+    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Feedback $feedback)
+    {
+        //
+    }
+}
