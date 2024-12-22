d3152cf2ea6a12f2d7ea2bc7adf6132208d22730
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-22 14:30:45 +0200

    define  feedbacks model relationship and mass assignment constraints

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Patient;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Feedback extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "message"
+    ];
+
+
+    // model relationships
+    public function patient(){
+        return $this->belongsTo(Patient::class);
+    }
 }
