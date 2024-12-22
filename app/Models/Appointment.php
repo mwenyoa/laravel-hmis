33f822b27632044110119f3a07f10a59930ee14c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-22 14:29:52 +0200

    define  apointment model relationships and mass assignment constraints

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use App\Models\Doctor;
+use App\Models\Patient;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "app_time",
+        "app_date",
+        "purpose"
+    ];
+
+    // model relationships
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
