f48ffcd7d12d9c42c2d982f67d89b83aee3a3e55
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-22 14:19:23 +0200

    Create API resourceful controller

---
+++ app/Http/Controllers/DoctorsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+
+class DoctorsController extends Controller
+{
+    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+  
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+
+
+   
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
