209fe79b362566b2b2bc1de34a555b1974b51b89
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-22 14:20:52 +0200

    Create users API resourceful controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use App\Http\Resources\UsersResourse;
+
+
+class UsersController extends Controller
+{
+
+    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
