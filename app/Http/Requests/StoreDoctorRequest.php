bfed9ece8aa6fc991907877241f4f45fe863be3f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-22 14:23:44 +0200

    Create doctors API request validator

---
+++ app/Http/Requests/StoreDoctorRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'specialization' => ['required', 'string', 'max:30', 'min:3'],
+            'hpcno' => ['required', 'string', 'max:30', 'min:3'],
+            'consultancy_fee' => ['required', 'float', 'max:30', 'min:3'],
+        ];
+    }
+}
