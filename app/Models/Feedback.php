9dead3543354a5fd8db9648be04d0167cad03b73
Anthony Mwenyo <mwenyoanthony@gmail.com>
2024-12-25 23:57:22 +0200

    Add Uuids to models

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Models\Patient;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Feedback extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
     protected $fillable = [
         "message"
     ];
