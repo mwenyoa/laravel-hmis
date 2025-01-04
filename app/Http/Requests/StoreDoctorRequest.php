19c90e3e2bc55c86fe4d8a5974fa2bfe24dd4c7a
Anthony Mwenyo <mwenyoanthony@gmail.com>
2025-01-04 08:23:14 +0200

    Make doctor's hpcno unique on doctors table

--- app/Http/Requests/StoreDoctorRequest.php
+++ app/Http/Requests/StoreDoctorRequest.php
@@ -23,7 +23,7 @@ class StoreDoctorRequest extends FormRequest
     {
         return [
             'specialization' => ['required', 'string', 'max:30', 'min:3'],
-            'hpcno' => ['required', 'string', 'max:30', 'min:3'],
+            'hpcno' => ['required', 'unique:doctors', 'string', 'max:30', 'min:3'],
             'consultancy_fee' => ['required',  'max:30', 'min:3'],
         ];
     }
