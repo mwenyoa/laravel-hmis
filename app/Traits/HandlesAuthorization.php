0ab612d2c245d07b4063d68693b74eca01eb8561
mwenyoa <mwenyoaanthony@gmail.com>
2025-01-06 16:11:48 +0200

    Create trait to handle user and resource authorization

---
+++ app/Traits/HandlesAuthorization.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Auth;
+
+trait HandlesAuthorization
+{
+    protected function ensureAuthenticated()
+    {
+        if (!Auth::check()) {
+            abort(401, 'You must be logged in to access this resource.');
+        }
+    }
+
+    protected function ensureOwnership($resource)
+    {
+        if (Auth::id() !== $resource->user_id) {
+            abort(403, 'You are not authorized to access this resource.');
+        }
+    }
+}
