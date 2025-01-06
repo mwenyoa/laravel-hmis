c211f42d7c0809d97e6afd79dbbee5d4905f558c
mwenyoa <mwenyoaanthony@gmail.com>
2025-01-06 16:08:24 +0200

    Update users authentication notification logic using a trait

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,11 +7,12 @@ use App\Models\User;
 use App\Traits\HttpResponses;
 use App\Http\Resources\UsersResource;
 use App\Http\Requests\UpdateUserRequest;
+use App\Traits\HandlesAuthorization;
 
 class UsersController extends Controller
 {
 
-    use HttpResponses;
+    use HttpResponses, HandlesAuthorization;
     /**
      * Display a listing of the resource.
      */
@@ -22,10 +23,7 @@ class UsersController extends Controller
     {
         try {
             // Check if the user is authenticated
-            if (!auth()->check()) {
-                return $this->error(null, "Please login to view", 401);
-            }
-
+            $this->ensureAuthenticated();
             // Fetch users and return them using the resource
             $users = UsersResource::collection(User::orderBy("created_at", "desc")->paginate(10));
             return $this->success($users, "Users Information", 200);
@@ -41,9 +39,7 @@ class UsersController extends Controller
     {
         try {
             // Check if the user is authenticated
-            if (!auth()->check()) {
-                return $this->error(null, "Unauthorized access", 401);
-            }
+            $this->ensureAuthenticated();
             $user_data = UsersResource::make($user);
             return $this->success($user_data, "$user->first_name $user->last_name Information");
         } catch (Exception $e) {
@@ -57,27 +53,14 @@ class UsersController extends Controller
     public function update(UpdateUserRequest $request, $id)
     {
         try {
-
-            if (!auth()->check()) {
-                return $this->error(null, "You are not authorized to update this data", 401);
-            }
-
+            $this->ensureAuthenticated();
             $user = User::findOrFail($id);
-
-            if ($user->id !== auth()->id()) {
-                return $this->error(null, "You are not authorized to update this record", 403);
-            }
-
+            $this->ensureOwnership($user);
             $validatedData = $request->validated();
-
             $user->update($validatedData);
-
             $formattedData = UsersResource::make($user);
-
-            // Return success response
             return $this->success($formattedData, "User data updated successfully", 200);
         } catch (Exception $e) {
-            // Handle exceptions and return error response
             return $this->error(null, $e->getMessage(), $e->getCode() ?: 500);
         }
     }
@@ -89,18 +72,10 @@ class UsersController extends Controller
     public function destroy(string $id)
     {
         try {
-            if (!auth()->check()) {
-                return $this->error(null, "You must be logged in to continue", 401);
-            }
-
+            $this->ensureAuthenticated();
             $user = User::findOrFail($id);
-
-            if (auth()->id() !== $user->id) {
-                return $this->error(null, "You're not permitted to delete this user's record", 403);
-            }
-
+            $this->ensureOwnership($user);
             $user->delete();
-
             return $this->success(null, null, 204);
         } catch (Exception $e) {
             return $this->error(null, $e->getMessage(), $e->getCode() ?: 500);
