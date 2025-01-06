5c97e4b4c85c9e8a85cdaef2cd3d0d2c4507e1a8
mwenyoa <mwenyoaanthony@gmail.com>
2025-01-06 16:10:11 +0200

    Validate age of user before insertion

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -26,6 +26,7 @@ class StoreUserRequest extends FormRequest
             'last_name' => ['required', 'string', 'max:30', 'min:3'],
             'phoneno' => ['required', 'string', 'max:30', 'min:3'],
             'gender' => ['required', 'string', 'max:30', 'min:3'],
+            'age' => ['required', 'integer', 'min:0'],
             'marital_status' => ['required', 'string', 'max:30', 'min:3'],
             'photo_url' => ['required', 'string', 'max:30', 'min:3'],
             'email' => ['required', 'string', 'max:50', 'unique:users'],
