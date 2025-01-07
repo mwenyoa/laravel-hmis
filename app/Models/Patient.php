dcb83f3d12bf84a0173701941f1a32da08e55a9e
Anthony Mwenyo <mwenyoanthony@gmail.com>
2025-01-07 23:32:54 +0200

    Add user method to the patient's model class

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,6 +14,10 @@ class Patient extends Model
     protected $fillable = [
         "home_address",
         "diagnosis",
+        "user_id"
     ];
 
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
