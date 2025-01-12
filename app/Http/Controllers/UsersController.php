e49df43d2b1e54e9bbd84dc2fc91fa7bd82c8cb2
Anthony Mwenyo <mwenyoanthony@gmail.com>
2025-01-13 00:48:00 +0200

    Refactor update method in users controller and error messages logic

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use Exception;
 use App\Models\User;
 use App\Traits\DebugError;
+use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
 use App\Traits\CustomErrorMessage;
 use App\Traits\HandlesAuthorization;
 use App\Http\Resources\UsersResource;
-use App\Http\Requests\UpdateUserRequest;
 
 class UsersController extends Controller
 {
@@ -24,12 +24,12 @@ class UsersController extends Controller
             // Check if the user is authenticated
             $this->ensureAuthenticated();
             // Fetch users and return them using the resource
-            $users = User::orderBy("created_at", "desc")->paginate(10); 
+            $users = User::orderBy("created_at", "desc")->paginate(10);
             $usersResource = UsersResource::collection($users);
             return $this->success($usersResource, "Users Information", 200);
         } catch (Exception $e) {
             $this->debugAppError($e);
-            $err_msg = $e->getMessage();;
+            $err_msg = $e->getMessage();
             return $this->error(null, $err_msg, 404);
         }
     }
@@ -46,7 +46,7 @@ class UsersController extends Controller
             return $this->success($user_data, "$user->first_name $user->last_name Information");
         } catch (Exception $e) {
             $this->debugAppError($e);
-            $err_msg = $e->getMessage();;
+            $err_msg = $e->getMessage();
             return $this->error(null, $err_msg, $e->getCode() ?: 422);
         }
     }
@@ -54,19 +54,21 @@ class UsersController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateUserRequest $request, $id)
+    public function update(Request $request, User $user)
     {
         try {
             $this->ensureAuthenticated();
-            $user = User::findOrFail($id);
+            if (!$user) {
+                return $this->error(null, "specified user not found", 404);
+            }
+            // verify resource ownership
             $this->ensureOwnership($user);
-            $validatedData = $request->validated();
-            $user->update($validatedData);
-            $formattedData = UsersResource::make($user);
-            return $this->success($formattedData, "User data updated successfully", 200);
+            $user->update($request->all());
+            $updated_data = UsersResource::make($user);
+            return $this->success($updated_data, "User data updated successfully", 200);
         } catch (Exception $e) {
             $this->debugAppError($e);
-            $err_msg = $e->getMessage();;
+            $err_msg = $e->getMessage();
             return $this->error(null, $err_msg, $e->getCode() ?: 500);
         }
     }
@@ -83,7 +85,7 @@ class UsersController extends Controller
             return $this->success(null, null, 204);
         } catch (Exception $e) {
             $this->debugAppError($e);
-            $err_msg = $e->getMessage();;
+            $err_msg = $e->getMessage();
             return $this->error(null, $err_msg, $e->getCode() ?: 500);
         }
     }
